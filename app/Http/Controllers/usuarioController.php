3262ef1bcd76b89e7487dfe6bf4da26245aa5a22
Camilo Andrés Echeverri Arias <camilo98423@hotmail.com>
2019-07-03 16:26:55 -0500

    Basic functions for usuarioController

--- app/Http/Controllers/usuarioController.php
+++ app/Http/Controllers/usuarioController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class usuarioController extends Controller
 {
-    //
+    /**
+     * Function to change something
+     *
+     * feature isn't done yet
+     *
+     * @param type var Description
+     * @return return type
+     */
+    public function __invoke()
+    {
+      // code...
+    }
 }
